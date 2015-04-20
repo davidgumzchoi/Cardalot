391708919ae2225a9f9a96bccd342c00d65c9f33
davidgumzchoi <davidgumzchoi@gmail.com>
2015-04-20 06:49:04 +0000

    first commit

--- js/application.js
+++ js/application.js
@@ -1,21 +1,10 @@
-// Images to load faster
-// Fix contact form
-
-$(function(){
-	$('#header').load('header.html'); 
-	$('#footer').load('footer.html'); 
-});
-
 // Document Window Height
 if (!window.matchMedia('(max-width: 375px)').matches) {
-	if ($('#home').length || $('#home2').length) {
-		$(window).load(function() {
-			var windowHeight = $(window).height();
-			var wrapperHeight = $('.wrapper').height();
-			var footerHeight = $('#footer').outerHeight();
-			if (windowHeight > wrapperHeight) {
-			  	$('.wrapper').css({'height':(windowHeight - footerHeight)+'px'});
-			}
-		})
-	}
+	$(window).load(function() {
+		var windowHeight = $(window).height();
+		var wrapperHeight = $('.wrapper').height();
+		if (windowHeight > wrapperHeight) {
+		  	$('.wrapper').css({'height':(windowHeight)+'px'});
+		}
+	})
 }
